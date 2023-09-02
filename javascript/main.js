74a292ac8420e340fd47dd3f5f1202167be2f85a
R4Tobi <R4Tobi@users.noreply.github.com>
2023-09-02 23:44:32 +0200

    finished projects

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,52 @@
+class Render {
+  async projects(id) {
+    var renderElem = document.getElementById(id);
+    var response = await fetch("https://api.github.com/users/r4tobi/repos");
+    var jsonData = await response.json();
+    var elems = [];
+    renderElem.innerHTML = `
+    <div class="banter-loader">
+      <div class="banter-loader__box"></div>
+      <div class="banter-loader__box"></div>
+      <div class="banter-loader__box"></div>
+      <div class="banter-loader__box"></div>
+      <div class="banter-loader__box"></div>
+      <div class="banter-loader__box"></div>
+      <div class="banter-loader__box"></div>
+      <div class="banter-loader__box"></div>
+      <div class="banter-loader__box"></div>
+    </div>`;
+    for (let index = 0; index < jsonData.length; index++) {
+      var releases = await fetch(
+        `https://api.github.com/repos/r4tobi/${jsonData[index].name}/releases`
+      );
+      var jsonReleases = await releases.json();
+      let releaseName = "no releases published yet";
+      if (jsonReleases.length > 0) {
+        releaseName = `[${jsonReleases[0].name}]`;
+      }
+      var elemStr = `<div class="card" onclick="window.open('${jsonData[index].html_url}')">
+                            <div class="card-content">
+                                <h3>${jsonData[index].name}</h3>
+                                <span>${releaseName}</span>
+                                <hr>
+                                <p>${jsonData[index].description}</p>
+                            </div>
+                        </div>`;
+      elems.push(elemStr)
+    }
+    renderElem.innerHTML = "";
+    elems.forEach((elem) => {
+      renderElem.innerHTML += elem;
+    });
+  }
+}
+
+function init() {
+  document.getElementById("nav").classList.remove("mobile-open");
+  cardHoverEffect();
+}
+
 function progressBarScroll() {
   let winScroll = document.body.scrollTop || document.documentElement.scrollTop,
     height =
@@ -11,49 +60,57 @@ window.onscroll = function () {
   progressBarScroll();
 };
 
-function activeLink(){
-  try{
+async function activeLink() {
+  try {
     document.getElementsByClassName("active")[0].classList.remove("active");
-  }
-  catch(e){
+  } catch (e) {
     e;
   }
   let current = window.location.toString();
-  if(window.location.pathname === "/" || window.location.pathname === ""){
-    current = window.location.toString() + "index.html"
+  if (window.location.pathname === "/" || window.location.pathname === "") {
+    current = window.location.toString() + "index.html";
   }
+  //functions called on side switch
+  if (window.location.pathname === "/projects.html") {
+    console.log("hello")
+    var render = new Render();
+    await render.projects("cards");
+    cardHoverEffect();
+  }
+  if(window.location.pathname === "/skills.html"){
+    cardHoverEffect();
+  }
+  //
   let elements = document.getElementsByClassName("nav-urls")[0].children;
   let index = 0;
   let length = elements.length;
 
-  for(index; index < length; index++){
-    if(elements[index].children[0].href === current){
-      elements[index].classList.add("active")
+  for (index; index < length; index++) {
+    if (elements[index].children[0].href === current) {
+      elements[index].classList.add("active");
     }
   }
 }
 
 activeLink();
 
-function toggleNav(){
+function toggleNav() {
   let nav = document.getElementById("nav");
-  nav.classList.toggle("mobile-open")
+  nav.classList.toggle("mobile-open");
 }
 
-function init(){
-  document.getElementById("nav").classList.remove("mobile-open");
-}
+function cardHoverEffect(){
+  const cards = document.querySelectorAll(".card");
+  const cardWrapper = document.querySelector(".cards");
 
-const cards = document.querySelectorAll(".card");
-const cardWrapper = document.querySelector(".cards");
+  cardWrapper.addEventListener("mousemove", function ($event) {
+    cards.forEach((card) => {
+      const rect = card.getBoundingClientRect();
+      const x = $event.clientX - rect.left;
+      const y = $event.clientY - rect.top;
 
-cardWrapper.addEventListener("mousemove", function ($event) {
-  cards.forEach((card) => {
-    const rect = card.getBoundingClientRect();
-    const x = $event.clientX - rect.left;
-    const y = $event.clientY - rect.top;
-
-    card.style.setProperty("--xPos", `${x}px`);
-    card.style.setProperty("--yPos", `${y}px`);
+      card.style.setProperty("--xPos", `${x}px`);
+      card.style.setProperty("--yPos", `${y}px`);
+    });
   });
-});
+}
